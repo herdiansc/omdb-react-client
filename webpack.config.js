06fc5aaf78ff66ebfa207860301a454a8f6814af
herdianovo <herdian@ovo.id>
2019-01-29 08:57:31 +0700

    webpack config build optimization

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,32 @@
 const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 var config = {
-   entry: __dirname + '/main.js',
+   entry: __dirname + '/src/main.js',
 	
    output: {
-      path: __dirname + '/',
-      filename: 'bundle.js',
+      path: __dirname + '/dist',
+      filename: '[name].[hash].bundle.js'
    },
 	
    devServer: {
+      contentBase: __dirname + "/public",
       inline: true,
       port: 8080
    },
 
    plugins: [
+      new HtmlWebpackPlugin({
+         template: __dirname + '/public/index.html'
+      }),
+      new MiniCssExtractPlugin({
+         filename: '[name].[hash].bundle.css'
+      }),
+      new CopyWebpackPlugin([
+         {from: __dirname + '/public/static', to: __dirname + '/dist/static'}
+      ]),
       new Dotenv()
    ],
 	
@@ -25,6 +38,19 @@ var config = {
             use: {
                loader: "babel-loader"
             }
+         },
+         {
+            test: /\.html$/,
+            use: {
+               loader: "html-loader"
+            }
+         },
+         {
+            test: /\.css/,
+            use: [
+               MiniCssExtractPlugin.loader,
+               "css-loader"
+            ]
          }
       ]
    }
