446dc40b20975261200b19903cb26b4656b693a7
herdianovo <herdian@ovo.id>
2019-01-29 08:57:52 +0700

    webpack config build optimization: add public and src folder

---
+++ src/components/MovieItemContainer.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { Link } from 'react-router';
+
+export default class MovieItemContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    };
+
+    image() {
+        if (!this.props.data.Poster || this.props.data.Poster != 'N/A') {
+            return <img src={ this.props.data.Poster } className="img-fluid" alt="" />
+        } else {
+            return;
+        }
+    }
+
+    render() {
+        return (
+                    <div className="card">
+                        <div className="view overlay hm-white-slight">
+                            { this.image() }
+                            <Link to={ `/detail/${this.props.data.imdbID}` }>
+                                <div className="mask"></div>
+                            </Link>
+                        </div>
+
+                        <div className="card-block">
+                            <h4 className="card-title">{ this.props.data.Title }</h4>
+                            <p className="card-text">Released: { this.props.data.Year }, Type: { this.props.data.Type }</p>
+                            <div className="read-more">
+                                <Link to={ `/detail/${this.props.data.imdbID}` } className='btn btn-primary'>Read more</Link>
+                            </div>
+                        </div>
+                    </div>
+        );
+    }
+}
---
+++ src/components/MovieListContainer.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+
+import { Link } from 'react-router';
+
+import MovieItemContainer from './MovieItemContainer.jsx';
+
+export default class MovieListContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.page = 1;
+
+        this.loadData = this.loadData.bind(this);
+    };
+
+    search(e) {
+        e.preventDefault();
+        if (this.refs.q.value.length != 0) {
+            this.props.router.push('/movies?q=' + this.refs.q.value);
+            this.loadData(this.refs.q.value, 1);
+        }
+    }
+
+    componentWillMount() {
+        this.page = this.props.location.query.page || this.page ; 
+        this.loadData(this.props.location.query.q, this.page);
+    }
+
+    loadData(q, page) {
+        scroll(0,0);
+        fetch(`${ process.env.REACT_APP_OMDB_API }?apikey=${ process.env.REACT_APP_OMDB_SECRET }&s=${ q }&page=${page}`)
+            .then(result=>result.json())
+            .then(items=>this.setState(items));
+    }
+
+    prevClick() {
+        this.page = Number(this.page) - 1;
+        this.loadData(this.props.location.query.q, this.page);
+    }
+
+    nextClick() {
+        this.page = Number(this.page) + 1;
+        this.loadData(this.props.location.query.q, this.page);
+    }
+
+    componentDidMount() {
+        this.refs.q.value = this.props.location.query.q;
+    }
+
+    pagination() {
+        return (
+            <div className="row pagination-container">
+                <nav className="row text-xs-center">
+                    { Number(this.page) > 1 ? <Link to={ `/movies?q=${this.props.location.query.q}&page=${ Number(this.page) - 1 }` } className="btn btn-sm btn-primary" onClick={this.prevClick.bind(this)}>Previous</Link> : null }
+                    <Link to={ `/movies?q=${this.props.location.query.q}&page=${ Number(this.page) + 1 }` } className="btn btn-sm btn-primary" onClick={this.nextClick.bind(this)}>Next</Link>
+                </nav>
+            </div>
+        );
+    }
+
+    render() {
+        return (
+            <div className="container container-300">
+                <div className="row search-form movie-list">
+                    <div className="col-md-12">
+                        <h2 className="page-title">Search</h2>
+                        <input type="text" placeholder="Enter Movie Title..." ref="q" />
+                        <button className="btn btn-primary btn-sm" type="submit" onClick={this.search.bind(this)} >Submit</button>
+                    </div>
+                </div>
+
+                { this.state.Search ? this.state.Search.map((item, i) => <MovieItemContainer key={i} data={item} /> ) : '' }
+
+                { this.state.totalResults>10 ? this.pagination() : '' }
+            </div>
+        );
+    }
+}
